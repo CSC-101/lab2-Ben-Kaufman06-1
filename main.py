7268f569d3203b17b423de6d58133459eea162cc
Ben Kaufman <bkaufm06@calpoly.edu>
2026-04-09 22:57:43 -0700

    First Commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Create a welcome message.
 # Input: a name as a string
 # Result: a string
-def welcome_message(name:str) -> str:
-   message = "Hello, " + name + "."
+def welcome_message(s:str) -> str:
+   message = "Hello, " + s + "."
 
    return message
 
